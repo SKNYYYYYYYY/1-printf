ea37194cd52c04f9f708541a3b97cf493c37e69d
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-26 20:13:08 +0300

    main.c

--- main.c
+++ main.c
@@ -9,31 +9,31 @@
  */
 int main(void)
 {
-//	int len, len2;
+	int len, len2;
 	char *str1, *str2;
-  long int j;
-
-    j = 12345678901;
-
-//	len = _printf("Let's try to printf a simple sentence.\n");
-//	len2 = printf("Let's try to printf a simple sentence.\n");
-
-	_printf("print character %c and %c\n", 'a', '%');
-	printf("print character %c and %c\n", 'a', '%');
+  int j;
 
 	str1 = "Newton";
 	str2 = "Abel";
-	_printf("print string %s and %s\n", str1, str2);
+  j = 1234567890;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+
+	_printf("_print character %c and %c\n", 'a', '%');
+	printf("print character %c and %c\n", 'a', '%');
+
+	_printf("_print string %s and %s\n", str1, str2);
 	printf("print string %s and %s\n", str1, str2);
 
-	_printf("Printed characters (custom): %i\n", j);
-	_printf("Printed characters (custom): %d\n", j);
-	_printf("Printed by _printf hexa with d: %d\n", 0xa);
-	printf("Printed characters (standard): %i\n", j);
-	printf("Printed characters (standard): %d\n", j);
-	printf("Printed by printf hexa with d: %d\n", 0xa);
-	_printf("Printed characters (standard): %i\n", 0xa);
-	printf("Printed characters (standard): %i\n", 0xa);
+	_printf("_Print integer: %i\n", j);
+	printf("Print integer: %i\n", j);
+	_printf("_Print decimal: %d\n", j);
+	printf("Print decimal: %d\n", j);
+	_printf("_print hexa with d: %d\n", 0xa);
+	printf("_print hexa with d: %d\n", 0xa);
+	_printf("_Print hexa with i: %i\n", 0xa);
+	printf("Print hexa with i: %i\n", 0xa);
     
     return (0);
 }
