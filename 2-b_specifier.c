81a060f4e945db78bc5d5b563fd6f3ebd5e4b508
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-26 21:35:25 +0300

    2-b_specifier.c updated

--- 2-b_specifier.c
+++ 2-b_specifier.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
 * b_specifier - converts an unsigned int to binary and prints it.
@@ -6,13 +7,14 @@
 *
 * Return: the number of characters printed.
 */
-int b_specifier(va_list args)
+	int b_specifier(va_list args)
 {
 	unsigned int num = va_arg(args, unsigned int);
-	char buffer[32]; /* Enough to hold 32-bit binary representation */
+	char *binary_str;
 	int printed_chars = 0;
-	int i = 0;
-	int leading_zero = 1;
+	int i, len = 0;
+	unsigned int temp = num;
+
 
 	if (num == 0)
 	{
@@ -20,22 +22,35 @@ int b_specifier(va_list args)
 		return (printed_chars);
 	}
 
-	while (num > 0)
+	while (temp > 0)
 	{
-		buffer[i++] = (num % 2) + '0';
+		len++;
+		temp /= 2;
+	}
+
+	binary_str = malloc(len + 1);
+	if (binary_str == NULL)
+		return (-1);
+
+	binary_str[len] = '\0';
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		binary_str[i] = (num % 2) + '0';
 		num /= 2;
 	}
 
-	while (i > 0)
+	int leading_zero = 1;
+
+	for (i = 0; i < len; i++)
 	{
-		if (buffer[i - 1] != '0')
+		if (binary_str[i] != '0')
 			leading_zero = 0;
 		if (!leading_zero)
-			printed_chars += _putchar(buffer[--i]);
-		else
-			i--;
+			printed_chars += _putchar(binary_str[i]);
 	}
+
+	free(binary_str);
 
 	return (printed_chars);
 }
-
