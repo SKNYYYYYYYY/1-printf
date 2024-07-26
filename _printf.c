0076f48fec1e37f3883697cfe89b5e3e7f1ecd1c
Abel Mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-26 09:29:17 +0000

    first structure

---
+++ _printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: is a character string composed of zero or more directives.
+ * handled conversion specifiers:
+ * 	- c
+ * 	- s
+ * 	- %
+ * 	- d
+ * 	- i
+ *
+ * Return: the number of characters printed.
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+fndef MAIN_H
+#define MAIN_H
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif /* NULL */
+
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
