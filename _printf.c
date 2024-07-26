3276cb31ecdc19bca539ebcc17507daabdd1847a
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-26 12:59:53 +0300

    main.h

--- _printf.c
+++ _printf.c
@@ -16,5 +16,40 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int i = 0; 
+	
+	va_start(args, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			if(format[i] == '%')
+			{
+				switch (format[i+1])
+				{
+					case 'c':
+						_printchar(va_arg(args, char));
+						i=i + 2;
+						break;
+					case 's':
+						_printstring(va_arg(args, char *));
+						break;
+					case '%':
+						_printnoarg(va_arg(args, char));
+						break;
+					default:
+						i++;
+						continue;
+
+				}
+			}
+			else
+				_putchar(format[i]);
+
+			i++;
+		}
+
+	}
 	return (0);
 }
